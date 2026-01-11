e79c6186c278e8ec3e52b41538dc94e71a87c140
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-01-11 10:33:23 +0530

    Add files via upload

---
+++ third.java
@@ -0,0 +1,9 @@
+public class third {
+    public static void main(String[] args) {
+        int a = 3, b = 4, temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + " " + b);
+    }
+}
