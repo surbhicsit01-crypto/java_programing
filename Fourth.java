f602f0a3d454cc38f5b6b2e68f8a8f99e136a8af
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-01-13 09:56:03 +0530

    Add files via upload

---
+++ Fourth.java
@@ -0,0 +1,9 @@
+public class Fourth {
+    public static void main(String args []){
+        int a=3 , b = 4;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println( a+ " "+b);
+    }
+}
