c708f4249298fac0865ce819de8844aaa0dc95c2
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-01-27 08:48:14 +0530

    Add files via upload

---
+++ sixth.java
@@ -0,0 +1,8 @@
+public class sixth {
+    public static void main(String args []){
+        char ch='A';
+        int num = 65;
+        System.out.println("ASCII of A:"+ (int)ch);
+        System.out.println("Charcterof 65: "+(char) num);
+    }
+}
