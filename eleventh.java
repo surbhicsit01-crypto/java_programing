36b3b7a94ee81dee8cc9777586f6c67cd10d172d
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-04-26 13:51:33 +0530

    Add files via upload

---
+++ eleventh.java
@@ -0,0 +1,15 @@
+class eleventh {
+    public static void main(String[] args) {
+        float f = 10.5f;
+
+        // Widening
+        double d = f;
+
+        // Narrowing
+        int i = (int) f;
+
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(i);
+    }
+}
